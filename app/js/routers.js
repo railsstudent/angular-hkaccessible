4fce207f910dcddffaf5e99e65128fd8a46d9896
Connie <cleung334@gmail.com>
2017-04-04 00:02:10 +0800

    Use a common html page for all 4 categories.

--- app/js/routers.js
+++ app/js/routers.js
@@ -3,7 +3,7 @@
 var  stateModule = angular.module('hkRouter', ['ui.router']);
 
 stateModule
-	.config(['$stateProvider', '$urlRouterProvider', 
+	.config(['$stateProvider', '$urlRouterProvider',
 	  function($stateProvider, $urlRouterProvider) {
 
 	    /////////////////////////////
@@ -13,52 +13,64 @@ stateModule
 
 	    // state provider
 	    $stateProvider
-	      .state('attractions', 
-	          { 
+	      .state('attractions',
+	          {
 	            url: '/attractions',
 	            templateUrl: 'partials/attraction.html',
 	            resolve: {
 	            	locationResult : [ 'Accessible', function(Accessible) {
 	            		return Accessible.get('facilities/attractions.json', 'attractions');
-	            	}]
+	            	}],
+								translateHeader: ['$translate', function($translate) {
+										return $translate('ATTRACTION');
+								}]
 	            },
 	            controller: 'AccessibleCtrl'
 	          })
-	      .state('shopping_dining', 
-	          { 
+	      .state('shopping_dining',
+	          {
 	            url: '/shopping_dining',
-	            templateUrl: 'partials/shopping.html',
+	            templateUrl: 'partials/attraction.html',
 	            resolve: {
 	            	locationResult : [ 'Accessible', function(Accessible) {
 	            		return Accessible.get('facilities/shoppings.json', 'shopping-dining');
-	            	}]
+	            	}],
+								translateHeader: ['$translate', function($translate) {
+										return $translate('SHOPPING');
+								}]
 	            },
 	            controller: 'AccessibleCtrl'
 	          })
-	      .state('hotels', 
-	          { 
+	      .state('hotels',
+	          {
 	            url: '/hotels',
-	            templateUrl: 'partials/hotel.html',
+	            templateUrl: 'partials/attraction.html',
 	            resolve: {
 	            	locationResult : [ 'Accessible', function(Accessible) {
 	            		return Accessible.get('facilities/hotels.json', 'hotels');
-	            	}]
+	            	}],
+								translateHeader: ['$translate', function($translate) {
+										return $translate('HOTEL');
+								}]
 	            },
 	            controller: 'AccessibleCtrl'
 	          })
-	      .state('other_venues', 
-	          { 
+	      .state('other_venues',
+	          {
 	            url: '/other_venues',
-	            templateUrl: 'partials/other.html',
+	            templateUrl: 'partials/attraction.html',
 	            resolve: {
 	            	locationResult : [ 'Accessible', function(Accessible) {
 	            		return Accessible.get('facilities/other_venues.json', 'other-venues');
-	            	}]
+	            	}],
+								translateHeader: ['$translate', function($translate) {
+										return $translate('OTHERVENUE');
+								}]
 	            },
 	            controller: 'AccessibleCtrl'
 	          })
-	      .state('geolocation', 
-	          { 
+	      .state('geolocation',
+	          {
 	            url: '/geolocation',
 	            templateUrl: 'partials/geolocation.html',
 	            resolve: {
@@ -68,7 +80,7 @@ stateModule
 	            },
 	            controller: 'MapCtrl'
 	          })
-	      .state('related_links', 
+	      .state('related_links',
 	        {
 	          url : '/links',
 	          templateUrl : 'partials/link.html',
