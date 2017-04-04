6db61b5626f1dc4f529f86cca04771e2d1d51998
Connie <cleung334@gmail.com>
2017-04-05 01:51:30 +0900

    Use controllerAs syntax and elminate $scope variable.

--- app/js/controller/controllers.js
+++ app/js/controller/controllers.js
@@ -4,13 +4,13 @@
 
 var moduleCtrl = angular.module('hkAccessibleControllers', []);
 
-moduleCtrl.controller("HomeCtrl", ["$scope", "$location", '$translate', '$rootScope',
-  function($scope, $location, $translate, $rootScope) {
-    $scope.isActive = function (viewLocation) {
+moduleCtrl.controller("HomeCtrl", ["$location", '$translate', '$rootScope',
+  function($location, $translate, $rootScope) {
+    this.isActive = function (viewLocation) {
         return viewLocation === $location.path();
     };
 
-    $scope.changeLanguage = function (langKey) {
+    this.changeLanguage = function (langKey) {
       $translate.use(langKey);
       // broadcast event to translation  the description in multiselect accessibility dropdown
       $rootScope.$broadcast('TRANSLATE_ACCESSIBILITY_DESC', langKey);
